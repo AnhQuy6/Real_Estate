ee57baea2edac8a99e3edd19fba2139bc79a9ff7
AnhQuy6 <hoangquy3125@gmail.com>
2024-12-16 10:41:47 +0700

    Exchange some folders

---
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-property-card',
+    standalone: true,
+    //template: `<h1>I am a car rrrr</h1>`,
+    templateUrl: 'property-card.component.html',
+    //styles: ['h1 {font-weight: normal; color: red;}']
+    styleUrl: 'property-card.component.css'
+}
+
+)
+export class PropertyCardComponent{}
